014292c4062005c799c7e74a5ee8964ad3386eb3
zax-pfe <axel75puech@gmail.com>
2025-09-19 11:07:30 +0200

    icon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/assets/icon/favicon.png" type="image/png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
