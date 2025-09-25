ab9b831b98731eb9ab8eff7d390a0d6d185abbb7
zax-pfe <axel75puech@gmail.com>
2025-09-25 21:55:17 +0200

    Adding text animation to hero phone page

--- src/components/phone/HeroPhone/HeroPhone.jsx
+++ src/components/phone/HeroPhone/HeroPhone.jsx
@@ -1,8 +1,13 @@
 import styles from "./style.module.scss";
 import { heroImages } from "@/data/imageList";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+
+import SplitText from "gsap/SplitText";
+import gsap from "gsap";
+import { IsLoadingContext } from "@/context/IsLoadingContext";
 const imageVariant = {
   hidden: {
     opacity: 0,
@@ -16,6 +21,10 @@ const imageVariant = {
   },
 };
 export default function HeroPhone() {
+  const textRef = useRef();
+  gsap.registerPlugin(SplitText);
+  const { isLoading, setIsLoading } = useContext(IsLoadingContext);
+
   const [activeHero, setActiveHero] = useState(0);
 
   useEffect(() => {
@@ -27,6 +36,30 @@ export default function HeroPhone() {
 
     return () => clearInterval(intervalId);
   }, [activeHero]);
+
+  useGSAP(() => {
+    document.fonts.ready.then(() => {
+      let split = SplitText.create(textRef.current, {
+        type: "chars, lines",
+        autoSplit: true,
+        mask: "lines",
+      });
+      const timeline = gsap.timeline();
+
+      if (isLoading === true) {
+        timeline.to({}, { duration: 1.7 }).from(split.chars, {
+          y: 100,
+          yPercent: 100,
+          ease: "power4.out",
+          stagger: {
+            amount: 1,
+            from: "random",
+          },
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className={styles.heroPhone}>
       <AnimatePresence mode="sync">
@@ -47,7 +80,7 @@ export default function HeroPhone() {
         </motion.div>
       </AnimatePresence>
 
-      <div className={styles.heroText}>
+      <div className={styles.heroText} ref={textRef}>
         <h1>Maxime</h1>
         <h1>Despiau</h1>
       </div>
